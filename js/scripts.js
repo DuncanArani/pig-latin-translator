ff0cf797405367d0137f60ba423aebf3ce6ba9f0
ARUNCO <duncanarani@gmail.com>
2018-07-11 18:02:34 +0300

    initial

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function TranslatePiglatin(string) {
+  var vowels=["a","b","c","d"],
+     results= str.split('');
+
+     if (vowels.include(string(0))) {
+       return string +='way';
+
+     }
+   }
